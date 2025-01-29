31091154e6e22e6f7ae9c05ac03f5022e632f193
urxpietrek <piotrowski1904@outlook.com>
2025-01-29 17:58:30 +0100

    clean up

--- run.py
+++ run.py
@@ -4,10 +4,3 @@ aplication = create_app()
 
 if __name__ == '__main__':
     aplication.run(debug=True)
-    
-"""
-twoej saldo poprawic: wyswietlanie i nie mozna dodac kosztow jesli saldo <
-przeznaczanie srodkow na cel
-wykres po srodku
-podliczanie wydatkow na kategorie
-baza danych do rodzajow wydatkow"""
